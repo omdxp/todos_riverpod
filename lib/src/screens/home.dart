9e17dc8ef24a9eec7a1f81f8af6f02c05b42a817
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-06-25 19:20:27 +0100

    Adding title widget.

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:todos_riverpod/src/utils/providers.dart';
 
-class Home extends StatelessWidget {
+class Home extends ConsumerWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ScopedReader watch) {
+    final todos = watch(filteredTodos);
     return Scaffold(
       appBar: AppBar(
         title: Text('Todos Riverpod'),
       ),
-      body: Container(),
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+        children: [],
+      ),
     );
   }
 }
---
+++ lib/src/widgets/title.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Title extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      'todo',
+      textAlign: TextAlign.center,
+      style: TextStyle(
+          color: Color.fromARGB(38, 47, 47, 247),
+          fontSize: 100,
+          fontWeight: FontWeight.w100,
+          fontFamily: 'Avenir'),
+    );
+  }
+}
