9ef6354933e46879e1323e3e7cba32db57f06e04
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-06-25 19:42:37 +0100

    Showing list of todos.

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -1,18 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todos_riverpod/src/utils/providers.dart';
+import 'package:todos_riverpod/src/widgets/title.dart';
+import 'package:todos_riverpod/src/widgets/todo_item.dart';
+import 'package:todos_riverpod/src/widgets/toolbar.dart';
 
 class Home extends ConsumerWidget {
   @override
   Widget build(BuildContext context, ScopedReader watch) {
     final todos = watch(filteredTodos);
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Todos Riverpod'),
-      ),
-      body: ListView(
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 40),
-        children: [],
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+          children: [
+            TitleWidget(),
+            const SizedBox(
+              height: 42,
+            ),
+            Toolbar(),
+            if (todos.isNotEmpty)
+              const Divider(
+                height: 0,
+              ),
+            for (var i = 0; i < todos.length; i++) ...[
+              if (i > 0)
+                const Divider(
+                  height: 0,
+                ),
+              ProviderScope(
+                  overrides: [currentTodo.overrideWithValue(todos[i])],
+                  child: TodoItem())
+            ]
+          ],
+        ),
       ),
     );
   }
--- lib/src/widgets/title.dart
+++ lib/src/widgets/title.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class Title extends StatelessWidget {
+class TitleWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Text(
